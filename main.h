09823040a42b116e757c35b4d0c4e0434a2c06f6
Augusto Ospal <aospalmadeo@itba.edu.ar>
2024-07-05 04:06:42 -0300

    Started to design the structure of the project

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef PROYECTO_FINAL_PI_MAIN_H
+#define PROYECTO_FINAL_PI_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+#define NUMBER_INPUT_FILES 2
+
+// Error codes
+#define ERROR_NUMBER_INPUT_FILES 1
+#define ERROR_EMPTY_FILE 2
+
+
+// Error messages
+#define ERROR_NUMBER_INPUT_FILES_M "Incorrect number of input files"
+#define ERROR_EMPTY_FILE_M "Theres at least one empty file"
+
+#endif //PROYECTO_FINAL_PI_MAIN_H
