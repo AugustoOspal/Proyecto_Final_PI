fcc8dcaa9a6b9d762d9fb0ccaa041ec0ea8ffc65
1ucasrn <lrodrigueznahuel@itba.edu.ar>
2024-07-05 17:20:35 -0300

    prueba

--- main.c
+++ main.c
@@ -5,9 +5,10 @@ int main(int argc, char *argv[])
 {
     validateCommandLine(argc);
     validateFiles(argc, argv);
-
+    int a;
     //Testing
     FILE *file = fopen(argv[1], "r");
     char buffer[2000] = {0};
     printf("%s", fgets(buffer, 2000, file));
 }
+
